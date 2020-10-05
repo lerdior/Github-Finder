22d36f9b2789820fb9e0c655b2a416d574b208cc
unknown <leon.paolovic@gmail.com>
2020-10-05 13:17:58 +0200

    initial commit to save the project

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import axios from "axios";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  //Search for GitHub users
+  searchUsers = async (text) => {
+    if (text === null || text === "") {
+      alert("Please enter something in the search.");
+    } else {
+      this.setState({ loading: true });
+
+      const res = await axios.get(
+        `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+        &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      );
+
+      this.setState({ users: res.data.items, loading: false });
+    }
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <div className='container'>
+          <Search searchUsers={this.searchUsers} />
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
